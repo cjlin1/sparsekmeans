51ade3b59994581f459f0c615fec9555e2a35c1f
khoinpd <nguyenphamdangkhoi2k@gmail.com>
2025-07-24 18:21:50 +0700

    Updated the package description on PyPI and included a link to the project's GitHub repository

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 PACKAGE_DIR = ["sparsekmeans"]
 PACKAGE_NAME = "sparsekmeans"
-VERSION = "0.2"
+VERSION = "0.2.1"
 
 
 # license parameters
@@ -17,8 +17,8 @@ setup(
     version=VERSION,
     python_requires=">=3.10",
     install_requires=["numpy","python-graphblas","scipy"],
-    description="",
-    long_description_content_type="",
+    description="A package for efficient implementation of the K-means clustering on high-dimensional sparse data sets",
+    long_description="See documentation here: https://github.com/cjlin1/sparsekmeans",
     author="Chih-Jen Lin, He-Zhe Lin, Khoi Nguyen Pham Dang",
     author_email="cjlin@csie.ntu.edu.tw",
     url="https://github.com/cjlin1/sparsekmeans",
